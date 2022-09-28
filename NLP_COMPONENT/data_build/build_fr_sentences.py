c23df06043af97583cc001982af376927e981981
RAMET Benjamin <ramet.benj@gmail.com>
2022-09-28 22:15:00 +0200

    Train NER pipeline and save it to disk

--- NLP_COMPONENT/data_build/build_fr_sentences.py
+++ NLP_COMPONENT/data_build/build_fr_sentences.py
@@ -8,7 +8,7 @@ from_part_of_speech_path = f"{base_path}from_part_of_speech.json"
 to_part_of_speech_path = f"{base_path}to_part_of_speech.json"
 locations_path = f"{base_path}locations.json"
 
-all_paths = [
+paths_config_1 = [
     root_sentence_path, 
     from_part_of_speech_path, 
     locations_path,
@@ -16,11 +16,19 @@ all_paths = [
     locations_path
 ]
 
+paths_config_2 = [
+    root_sentence_path, 
+    to_part_of_speech_path, 
+    locations_path,
+    from_part_of_speech_path, 
+    locations_path
+]
+
 all_fr_sentences = []
 
-for i in range(3000):
+for i in range(200):
     full_sentence = ""
-    for path in all_paths:
+    for path in paths_config_2:
         with open(path, "r", encoding="utf-8") as f:
             read_file = json.load(f)
             random_n = random.randint(1,len(read_file) - 1)
@@ -30,5 +38,5 @@ for i in range(3000):
     all_fr_sentences.append(full_sentence)
 
 for fr_sentence in all_fr_sentences:
-    with open("data/lang/fr.txt", "a+", encoding="utf-8") as outfile:
+    with open("data/lang/fr_v2.txt", "a+", encoding="utf-8") as outfile:
         outfile.write(fr_sentence + "\n\n")
